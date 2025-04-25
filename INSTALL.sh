2de606ebdaec62c0e077f450e9202fb9cb93e941
VPSHOZOO <lordhozoo891@gmail.com>
2025-04-25 12:47:53 +0700

    Create INSTALL.sh

---
+++ INSTALL.sh
@@ -0,0 +1,17 @@
+sudo apt-get install nodejs -y
+
+clear
+
+sudo apt-get install npm -y
+clear
+npm instal fs
+clear
+npm install axios
+clear
+npm install node-telegram-bot-api
+clear
+npm install path
+
+node HOZOO.js
+
+
